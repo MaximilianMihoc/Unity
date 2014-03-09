2b6da2ef2e8685d564f52da95f2414ab35a3ae47
Maximilian Mihoc <maximilian.mihoc@mydit.ie>
2014-03-09 17:36:51 +0000

    Change Time script

--- NinjaGame/Assets/Scripts/TimeText.cs
+++ NinjaGame/Assets/Scripts/TimeText.cs
@@ -5,17 +5,17 @@ public class TimeText : MonoBehaviour {
 
 	public GUIText timerText;
 	//my COUNTDOWN timer
-	public float timer = 30.00f;
+	public float timer = 0.0f;
 
 	void Update () {
-		timer -= Time.deltaTime;
+		timer += Time.deltaTime;
 
 		timerText.text = "Time: " + timer.ToString("0.00");
 
-		if (timer <= 0)
+		/*if (timer <= 0)
 		{
 			timerText.text = "";
 			Time.timeScale = 0;
-		}
+		}*/
 	}
 }

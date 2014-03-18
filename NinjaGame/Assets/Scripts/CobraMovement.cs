96552b935a98769c4cf26d4f955ae1776c7b232d
Maximilian Mihoc <maximilian.mihoc@mydit.ie>
2014-03-18 12:52:42 +0000

    Player Sword Hit & Cobra movement

--- NinjaGame/Assets/Scripts/CobraMovement.cs
+++ NinjaGame/Assets/Scripts/CobraMovement.cs
@@ -3,16 +3,35 @@ using System.Collections;
 
 public class CobraMovement : MonoBehaviour {
 
-    public float speed = -0.1f;
+    public float speed = -0.9f;
+    bool flip = true;
+    float time = 0;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+	void FixedUpdate () 
+    {
         
         rigidbody2D.velocity = new Vector2(speed, rigidbody2D.velocity.y);
 	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag != "Platform"  && flip)
+        {
+            speed = speed * (-1);
+            Flip();
+            flip = false;
+            time = Time.time;
+        }
+        if (time + 0.2 < Time.time)
+            flip = true;
+        
+    }
+
+    void Flip()
+    {
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
 }
---
+++ NinjaGame/Assets/Scripts/SwordController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwordController : MonoBehaviour {
+
+    void Start()
+    {
+        collider2D.enabled = false;
+    }
+	
+	void Update () 
+    {
+	    if(Input.GetKeyDown(KeyCode.Space))
+        {
+            collider2D.enabled = true;
+        }
+        else collider2D.enabled = false; 
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Cobra")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
